4732e5fc2ba5b786c0a549d7e9bea75544fe523e
ghatikawaykole <ghatika99@gmail.com>
2021-09-09 11:31:02 +0530

    snack and ladder program player at initial position

---
+++ src/com/example/SnackAndLadderUC1/SnackAndLadder.java
@@ -0,0 +1,11 @@
+package com.example.SnackAndLadderUC1;
+
+public class SnackAndLadder {
+   public static void main(String args[]){
+       // snack and the ladder program
+      final int player;
+      final int pos=0;
+       System.out.println("Player is at start position");
+
+   }
+}
