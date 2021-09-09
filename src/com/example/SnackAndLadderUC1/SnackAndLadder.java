4efab3f76eb893464a75c0536f28b5ff0b96e221
ghatikawaykole <ghatika99@gmail.com>
2021-09-09 12:08:32 +0530

    snack and ladder with noPlay,snack and ladder compute

--- src/com/example/SnackAndLadderUC1/SnackAndLadder.java
+++ src/com/example/SnackAndLadderUC1/SnackAndLadder.java
@@ -1,14 +1,40 @@
 package com.example.SnackAndLadderUC1;
 
 public class SnackAndLadder {
+
    public static void main(String args[]){
        // snack and the ladder program
       final int player;
-      final int pos=0;
+      final int NO_PLAY = 0;
+      final int SNACK = 1;
+      final int Ladder = 2;
+       // variable
+       int pos = 0;
        System.out.println("Snack and ladder program");
        System.out.println("Player is at start position");
        int die = (int) Math.floor(Math.random() *6 ) +1;
        System.out.println("Number the player give"+die);
+       int die1 = (int) Math.floor(Math.random() *3 ) ;
+       switch(die1){
+           case NO_PLAY:System.out.println("Player does not play");
+                        pos = pos;
+                        System.out.println("Player at position"+pos);
+           break;
+           case SNACK: System.out.println("Player get the snack");
+               pos -= die;
+                  if(pos <0) {
+                      pos = 0;
+                      System.out.println("Player at position" + pos);
+                  }
+                  else {
+                      System.out.println("Player at position" + pos);
+                  }
+               break;
+           case Ladder: System.out.println("Player get the ladder");
+                        pos += die;
+                        System.out.println("Player at position"+die1);
+               break;
+       }
 
    }
 }
